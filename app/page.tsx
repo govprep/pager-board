c2b09283ca18ef12f50e2834d755ed6543815600
Element <97992918+IAmElement@users.noreply.github.com>
2026-07-19 13:56:11 +1000

    Revert "Add landing intro splash: sound + spinning logo, fades into the site"
    This reverts commit 9a2054905ea337bedb5db6c92aebc6eae0335307.

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import AccessGate from "@/components/AccessGate";
-import IntroGate from "@/components/IntroGate";
 
 export const dynamic = "force-dynamic";
 
@@ -10,9 +9,5 @@ export const dynamic = "force-dynamic";
 // the device's invite token exchanges for a valid session; the board then loads
 // incidents via the authed /api/incidents fetch.
 export default function Page() {
-  return (
-    <IntroGate>
-      <AccessGate />
-    </IntroGate>
-  );
+  return <AccessGate />;
 }
